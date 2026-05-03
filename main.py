11d31fcf3c225f887ade6f885c2f870630930c74
Sameer Bansal <sameerbansal2005@gmail.com>
2026-05-03 09:29:21 +0530

    feat: Add initial project structure and main script

---
+++ main.py
@@ -0,0 +1,11 @@
+# password-generator
+# Author: Sameer Bansal
+# SRM Institute of Science and Technology
+
+def main():
+    print("Password Generator built with Python")
+    print("Project: password-generator")
+    print("Author: Sameer Bansal | RA2311032010061")
+
+if __name__ == "__main__":
+    main()
